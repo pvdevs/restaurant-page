f45162e44f99f023c38826008046aa5cc8b30a10
Paulo Victor <paulovictor.devs@gmail.com>
2023-09-20 19:36:20 -0300

    add about page and its designs

--- src/about.js
+++ src/about.js
@@ -1,12 +1,25 @@
 function about() {
-    const textContainer = document.createElement('div');
-    const text = document.createElement('p');
+    const page = document.createElement('div');
+    const quoteContainer = document.createElement('div');
+    const quote = document.createElement('p');
+    const author = document.createElement('p');
     const logo = document.createElement('img');
 
-    logo.setAttribute('src', '../src/img/logo.svg');
-    text.textContent = 'Testing testin test';
+    page.classList.add('page');
+    quoteContainer.classList.add('quote-container')
+    logo.classList.add('logo');
+    quote.classList.add('quote-text');
+    author.classList.add('author');
 
-    return text;
+    logo.setAttribute('src', '../src/img/logo.svg');
+
+    quote.textContent = '“Lorem ipsum dolor sit amet consectetur. Libero quis tristique faucibus arcu quam ut ut ipsum. Scelerisque amet ac dictumst lectus id massa diam velit diam. Tristique fusce quam tristique commodo rhoncus eleifend neque. Amet netus dignissim morbi eu a. Pellentesque congue vitae ac ornare tincidunt ullamcorper sit faucibus.”';
+    author.textContent = '-Phabin';
+
+    quoteContainer.append(quote,author);
+    page.append(logo,quoteContainer);
+
+    return page;
 }
 
 export default about;
