7e5e629e1130d89415e6ef54a59ee59636443d3a
Paulo Victor <paulovictor.devs@gmail.com>
2023-09-20 17:58:17 -0300

    add switch page logic

---
+++ src/about.js
@@ -0,0 +1,10 @@
+function about() {
+    const textContainer = document.createElement('div');
+    const text = document.createElement('p');
+
+    text.textContent = 'Testing testin test';
+
+    return text;
+}
+
+export default about;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,77 @@
-import pageLoad from './page-load'
-import menu from './menu'
+import pageLoad from './page-load';
+import about from './about';
+//import {menu, getSelectedTab} from './menu';
 
 const content = document.getElementById('content');
-content.appendChild(menu());
-
-content.appendChild(pageLoad());
 
 
+createPage(content,pageLoad());
 
-console.log('Works');
+
+let selected;
+
+function nav(){
+    const topNav = document.createElement('div');
+    const homeTab = document.createElement('button');
+    const menuTab = document.createElement('button');
+    const aboutTab = document.createElement('button');
+
+    topNav.classList.add('top-menu');
+    homeTab.classList.add('menu-tabs');
+    menuTab.classList.add('menu-tabs');
+    aboutTab.classList.add('menu-tabs');
+
+    homeTab.textContent = 'Home';
+    menuTab.textContent = 'Menu';
+    aboutTab.textContent = 'About';
+
+    homeTab.addEventListener('click', e => {
+        homeTab.classList.add('selected');
+        menuTab.classList.remove('selected');
+        aboutTab.classList.remove('selected');
+        selected = 'Home';
+        switchPage(selected)
+    });
+
+    menuTab.addEventListener('click', e => {
+        menuTab.classList.add('selected');
+        homeTab.classList.remove('selected');
+        aboutTab.classList.remove('selected');
+        selected = 'Menu';
+    })
+
+    aboutTab.addEventListener('click', e => {
+        aboutTab.classList.add('selected');
+        homeTab.classList.remove('selected');
+        menuTab.classList.remove('selected');
+        selected = 'About';
+        switchPage(selected)
+    })
+
+    topNav.append(homeTab,menuTab,aboutTab);
+    return topNav;
+}
+
+function switchPage(tab){
+    switch (tab) {
+        case 'Home':
+            createPage(content, pageLoad());
+            break;
+        case 'About':
+            createPage(content, about());
+        default:
+            break;
+    }
+}
+
+function removeAllChildren(parent){
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function createPage(parent, newPage) {
+    removeAllChildren(parent);
+    parent.appendChild(nav());
+    parent.appendChild(newPage);
+}
